db4b956e0f66a5e07f94aa69f3965a881ae193f9
DhaniCuPliZ <ramadhanirafli065@gmail.com>
2024-05-27 10:18:26 +0700

    buat class remoteLampu, private saklarNo, agar bisa dipanggil buat function setSaklarNo dan getSaklarNo dengan public

---
+++ Enkapsulasi/Enkapsulasi.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+class remoteLampu {
+private:
+	string saklarNo[10];
+public:
+	void setSaklarNo(int i, string value) {
+		saklarNo[i] = value;
+	}
+	string  getSaklarNo(int i, string value) {
+		return saklarNo[i];
+
+	}
+
+};
