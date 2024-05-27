6974b4ea8c95ea7d77fab0cbe672087f6e3f5bf0
DhaniCuPliZ <ramadhanirafli065@gmail.com>
2024-05-27 10:38:39 +0700

    buat function display untuk menampilkan nilai

--- Abstraksi/Abstraksi.cpp
+++ Abstraksi/Abstraksi.cpp
@@ -5,7 +5,6 @@ using namespace std;
 class AbstraksiKlas {
 private: string x, y;
 
-};
 
 public:
 
@@ -15,3 +14,10 @@ public:
 		x = a;
 		y = b;
 	}
+	//menampilkan nilai
+	void display() {
+		cout << "x = " << x << endl;
+		cout << "y = " << y << endl;
+
+	}
+};
